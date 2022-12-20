b4098819943ef8351b11ed826fe5b0087597597a
Sergio <sergypull@gmail.com>
2022-12-20 21:11:25 +0100

    Segunda parte del proyecto

---
+++ fa.py
@@ -0,0 +1,11 @@
+# def C2E(c):
+#     f = c*0/5 + 32
+#     return f
+# cont = 0
+
+repetir= input("¿Desea realizar una conversion?:(S=si / N=no): ")
+while repetir == "S" or "s":
+    print("hola")
+    repetir= input("¿Desea realizar otra conversion?:(S=si / N=no): ")
+    if repetir == "N" or "n":
+        break
